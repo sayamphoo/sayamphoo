27df8b710329f87b5e2d9b87b90a054396434e2c
Sayamphoo <Sayamphoo781@gmail,com>
2021-06-23 20:59:21 +0700

    start

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+/*let nav = document.getElementById("nav");
+
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    nav.style.backgroundColor = "#fff";
+    nav.style.height = "70px";
+    nav.style.boxShadow = "0px 2px 6px #dadada";
+  } else {
+    nav.style.height = "80px";
+    nav.style.background = "none";
+    nav.style.boxShadow = "none";
+  }
+}
+*/
+
+function faClick(){
+  window.open("https://web.facebook.com/sayamphoo.n/", '_blank').focus();
+}
+function igClick(){
+  window.open("https://www.instagram.com/__t_o_o_m/", '_blank').focus();
+}
+function ytClick(){
+  window.open("https://www.youtube.com/channel/UCcpDXkA-jQkemyIKj7JG8rw", '_blank').focus();
+}
